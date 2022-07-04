131b8deff46a2f81c41f85ebea023ea5b8f69eba
Andrew Gomez <drewgomez209@gmail.com>
2022-07-04 12:36:00 -0700

    message search finished

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const menuItems = document.querySelectorAll('.menu-item');
 //MESSAGES
 const messagesNotification = document.querySelector('#messages-notification')
 const messages = document.querySelector('.messages');
-
+const message = messages.querySelectorAll('.message');
+const messageSearch = document.querySelector('#message-search')
 
 // remove active class from all menu items
 const changeActiveItem = () => {
@@ -31,10 +32,27 @@ menuItems.forEach(item => {
 
 // ================ MESSAGES =================
 
+// searches chat
+const searchMessage = () => {
+    const val = messageSearch.value.toLowerCase();
+    message.forEach(chat => {
+        let name = chat.querySelector('h5').textContent.toLowerCase();
+        if (name.indexOf(val) != -1) {
+            chat.style.display = 'flex';
+        } else {
+            chat.style.display = 'none'
+        }
+    })
+}
+
+// search chat
+messageSearch.addEventListener('keyup', searchMessage)
+
+// highlight messages card when message menu is clicked
 messagesNotification.addEventListener('click', () => {
-    messages.style.boxShadow = '0 0 1rem var(--color-primary)';
-    messages.messagesNotification.querySelector('.notification-count').style.display = 'none'
-    setTimeout(() => {
-        messages.style.boxSHadow = 'none';
-    }, 2000)
+    // messages.style.boxShadow = '0 0 1rem var(--color-primary)';
+    // messages.messagesNotification.querySelector('.notification-count').style.display = 'none'
+    // setTimeout(() => {
+    //     messages.style.boxSHadow = 'none';
+    // }, 2000)
 })
