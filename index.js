258fff8b76ba6651b6f4d84ea9a712747357a3f5
Andrew Gomez <drewgomez209@gmail.com>
2022-07-05 23:51:27 -0700

    initial theme updated

--- index.js
+++ index.js
@@ -15,6 +15,9 @@ const themeModal = document.querySelector('.customize-theme');
 const fontSizes = document.querySelectorAll('.choose-size span');
 var root = document.querySelector(':root');
 const colorPalette = document.querySelectorAll('.choose-color span')
+const Bg1 = document.querySelector('.bg-3')
+const Bg2 = document.querySelector('.bg-2')
+const Bg3 = document.querySelector('.bg-1')
 
 // remove active class from all menu items
 const changeActiveItem = () => {
@@ -125,15 +128,23 @@ fontSizes.forEach(size => {
     })
 })
 
+// remove active class from colors
+const changeActiveColorClass = () => {
+    colorPalette.forEach(colorPicker => {
+        colorPicker.classList.remove('active')
+    })
+}
+
+
 // change primary colors
 colorPalette.forEach(color => {
     color.addEventListener('click', () => {
         let primary;
-
+        changeActiveColorClass()
         if (color.classList.contains('color-1')) {
-            primaryHue = 252;
-        } else if (color.classList.contains('color-2')) {
             primaryHue = 52;
+        } else if (color.classList.contains('color-2')) {
+            primaryHue = 252;
         } else if (color.classList.contains('color-3')) {
             primaryHue = 352;
         } else if (color.classList.contains('color-4')) {
@@ -141,9 +152,73 @@ colorPalette.forEach(color => {
         } else if (color.classList.contains('color-5')) {
             primaryHue = 202;
         }
+        color.classList.add('active')
 
         root.style.setProperty('--primary-color-hue', primaryHue)
     })
 })
+
+
+// THEME BACKGROUND VALUES
+let lightColorLightness;
+let whiteColorLightness;
+let darkColorLightness;
+
+const changeBG = () => {
+    root.style.setProperty('--light-color-lightness', lightColorLightness);
+    root.style.setProperty('--white-color-lightness', whiteColorLightness);
+    root.style.setProperty('--dark-color-lightness', darkColorLightness);
+
+    // get and change color of all primary buttons colors
+    let primaryButtons = document.querySelectorAll('btn-btn-primary');
+    primaryButtons.forEach(button => { button.style.color = 'white' })
+
+    // get and change color of all story names
+    let storyNames = document.querySelectorAll('.stories .story p')
+    storyNames.forEach(name => {
+        name.style.color = 'white'
+    })
+}
+
+Bg1.addEventListener('click', () => {
+    // add active class
+    Bg3.classList.add('active');
+    // remove active class from the others
+    Bg2.classList.remove('active');
+    Bg1.classList.remove('active');
+    // remove customized changes from local storage
+    window.location.reload()
+})
+
+Bg2.addEventListener('click', () => {
+    darkColorLightness = '95%';
+    whiteColorLightness = '20%';
+    lightColorLightness = '15%';
+
+    // add active class
+    Bg2.classList.add('active');
+    // remove active class from the others
+    Bg1.classList.remove('active');
+    Bg3.classList.remove('active');
+    // changeBG()
+    // remove customized changes from local storage
+    changeBG()
+})
+
+Bg3.addEventListener('click', () => {
+    darkColorLightness = '17%';
+    whiteColorLightness = '100%';
+    lightColorLightness = '95%';
+
+    // add active class
+    Bg1.classList.add('active');
+    // remove active class from the others
+    Bg2.classList.remove('active');
+    Bg3.classList.remove('active');
+    changeBG()
+})
+
+// END
+
 
 
